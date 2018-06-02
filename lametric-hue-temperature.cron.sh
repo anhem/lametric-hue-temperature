e8177120ff755fcd254032a7226152a36b9a8828
Andreas Hemmingsson <andhem@gmail.com>
2018-06-02 22:31:32 +0200

    added sh script for cron job

---
+++ lametric-hue-temperature.cron.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+HUE_API_KEY=<key> \
+HUE_BRIDGE_IP=<ip> \
+LAMETRIC_ACCESS_TOKEN=<token> \
+LAMETRIC_PUSH_URL=<url> \
+/usr/local/bin/node <path>/lametric-hue-temperature/dist/app.js 2>&1
